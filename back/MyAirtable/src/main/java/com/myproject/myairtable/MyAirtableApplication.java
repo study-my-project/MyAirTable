e6784903337a60124e8db2fe567468aa95984e8c
mineki <mineki94@naver.com>
2024-11-06 17:16:55 +0900

    # 백엔드 프로젝트 생성
    신규 프로젝트 생성하기

---
+++ back/MyAirtable/src/main/java/com/myproject/myairtable/MyAirtableApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.myairtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAirtableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAirtableApplication.class, args);
+    }
+
+}
---
+++ back/MyAirtable/src/test/java/com/myproject/myairtable/MyAirtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.myairtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAirtableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
